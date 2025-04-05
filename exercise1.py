d25fbbd445cc3948664d9037aef32712e35ce092
Leon <kyawtunlwin.l@gmail.com>
2025-04-05 21:05:23 +1100

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,15 @@
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Describe the Problem - Write your answers as comments:
+# 1. What is the for loop doing?
+# it will loop 20 times
+# 2. When is the function meant to print "You got it"?
+# if i is equal to 20, it will print "You got it".
+# 3. What are your assumptions about the value of i?
+# range function range(1,20), will only loop from 1 to 19 (exclusive of 20)
